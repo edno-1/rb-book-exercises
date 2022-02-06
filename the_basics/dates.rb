b61994bf810da129864ef0a954bdb21ff457086b
Edward No <no.edward1@gmail.com>
2022-02-06 06:08:47 +0000

    Add rb exercise files

---
+++ the_basics/dates.rb
@@ -0,0 +1,2 @@
+dates = [1945, 2009, 2017, 2022]
+dates.each { |x| puts x}
---
+++ the_basics/movie_title_hash.rb
@@ -0,0 +1,7 @@
+movies = {
+  :johnny => "1975",
+  :catcat => "2018",
+  :johncat => "2003",
+  :catdog => "2022"}
+  
+movies.each { |x, y| puts y}
